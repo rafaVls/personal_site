5238eddbfa53d28c4b216941ea94a832e1a1bd73
Rafael Avilés <rafa.vfierro@outlook.com>
2023-06-06 17:46:20 -0700

    feat: add safeguard if project object doesn't have a website property

--- src/ProjectCard.ts
+++ src/ProjectCard.ts
@@ -24,19 +24,13 @@ class ProjectCard {
     this._title.textContent = title;
     this._description.textContent = description;
     this._stack.textContent = stack.join(", ");
-    this._repoAnchor.setAttribute("href", repoHref);
-    this._websiteAnchor.setAttribute("href", websiteHref);
     this._card.style.backgroundImage = `url(${imageUrl})`;
-
-    this._repoAnchor.setAttribute("target", "_blank");
-    this._websiteAnchor.setAttribute("target", "_blank");
-
     this._stackTitle.textContent = "Stack";
-    this._repoAnchor.textContent = "Code";
-    this._websiteAnchor.textContent = "Website";
+
+    this.setupAnchor("Website", websiteHref, this._websiteAnchor);
+    this.setupAnchor("Repo", repoHref, this._repoAnchor);
 
     this._card.append(this._info);
-    this._links.append(this._repoAnchor, this._websiteAnchor);
     this._info.append(
       this._title,
       this._description,
@@ -44,6 +38,17 @@ class ProjectCard {
       this._stack,
       this._links
     );
+  }
+
+  setupAnchor(type: "Website" | "Repo", href: string, anchorElement: HTMLElement) {
+    if (!href) {
+      return;
+    }
+
+    anchorElement.textContent = type;
+    anchorElement.setAttribute("href", href);
+    anchorElement.setAttribute("target", "_blank");
+    this._links.append(anchorElement);
   }
 
   createCard() {
